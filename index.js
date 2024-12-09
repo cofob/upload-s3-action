870884245b44b301ab471fc89ebc6607924c9d75
Egor Ternovoy <cofob@riseup.net>
2024-12-09 14:27:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if (ENDPOINT) {
 }
 
 const s3 = new S3(s3options);
-const destinationDir = DESTINATION_DIR === '/' ? shortid() : DESTINATION_DIR;
+const destinationDir = DESTINATION_DIR;
 const paths = klawSync(SOURCE_DIR, {
   nodir: true,
 });
